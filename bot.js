d4620cd0b4e5cac51e337eb1a55289d44d5466df
7sena <40254036+7sena@users.noreply.github.com>
2018-10-06 14:47:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,7 +158,7 @@ client.on('ready', () => {
   console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client3.user.username} " ]`);
+  console.log(`Logged in as * [ " ${} " ]`);
   console.log('')
   console.log('Informations :')
   console.log('')
