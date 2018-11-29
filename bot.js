80dc9c6d0199b27d652a666a97a061e29f3dc935
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-29 21:30:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,4 +13,20 @@ client.on('ready', async() => {
     },205);
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
 client.login("NTA1OTk1MDA0ODYzMzE1OTY4.DrbsPA.k-U2zG1x3aDuJZ4H0EOx8UfyK8w");
